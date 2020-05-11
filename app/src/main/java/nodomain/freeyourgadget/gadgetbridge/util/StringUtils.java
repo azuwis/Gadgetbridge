2ec0e42f5cca592ac67fbd04aaa4cd3620731752
Zhong Jianxin <azuwis@gmail.com>
2020-05-11 15:56:52 +0800

    Add StringUtils.utf8truncate

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/util/StringUtils.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/util/StringUtils.java
@@ -37,6 +37,35 @@ public class StringUtils {
         return s.substring(0, length);
     }
 
+    public static String utf8truncate(String input, int length) {
+        StringBuffer result = new StringBuffer(length);
+        int resultlen = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            int charlen = 0;
+            if (c <= 0x7f) {
+                charlen = 1;
+            } else if (c <= 0x7ff) {
+                charlen = 2;
+            } else if (c <= 0xd7ff) {
+                charlen = 3;
+            } else if (c <= 0xdbff) {
+                charlen = 4;
+            } else if (c <= 0xdfff) {
+                charlen = 0;
+            } else if (c <= 0xffff) {
+                charlen = 3;
+            }
+            if (resultlen + charlen - 3 > length) {
+                result.append("...");
+                break;
+            }
+            result.append(c);
+            resultlen += charlen;
+        }
+        return result.toString();
+    }
+
     public static String pad(String s, int length){
         return pad(s, length, ' ');
     }
